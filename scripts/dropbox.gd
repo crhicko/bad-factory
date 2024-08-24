a6f60a78caab26b74f37815396d186b35ab6acba
crhicko <crhicko@protonmail.com>
2024-08-24 16:47:07 -0400

    fix buggarinos

--- scripts/dropbox.gd
+++ scripts/dropbox.gd
@@ -1,19 +1,15 @@
-extends Node
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_viewport().set_physics_object_picking_first_only(true)
-	get_viewport().set_physics_object_picking_sort(true)
-
-
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-#func drop(body: Node2D, mouse_pos: Vector2) -> void:
-	#add_child(body)
-	#body.position = mouse_pos
+
+##propagate to the actual belt node
+func handle_drop(target: Node, body: Node2D, mouse_pos: Vector2) -> void:
+	get_parent().handle_drop(target, body, mouse_pos)
