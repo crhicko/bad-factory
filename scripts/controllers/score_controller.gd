f33a68ef3cdf7a185941804a72ea383444240821
crhicko <crhicko@protonmail.com>
2024-08-24 17:45:02 -0400

    basic scoring

--- scripts/controllers/score_controller.gd
+++ scripts/controllers/score_controller.gd
@@ -1,14 +1,17 @@
 extends Node
 class_name ScoreController
 
-@export_group("Controllers")
-@export var ShapeSpawner: Node
+@export_group("Required Nodes")
+@export var shape_spawner: ShapeSpawner
+
 
 var score: int = 0
 
+signal score_changed(amount)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ShapeSpawner.spawned.connect(_on_spawned)
+	shape_spawner.spawned.connect(_on_spawned)
 	pass # Replace with function body.
 
 
@@ -23,6 +26,6 @@ func _on_spawned(entity: Node2D) -> void:
 		entity.died.connect(_on_died)
 		
 func _on_died(amount: int):
-	print("bro died")
 	score += amount
-	print(score)
+	print("Score: ", score)
+	score_changed.emit(score)
